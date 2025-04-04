d164758c1fd42ce57b16da99add2a3694c2b7d7d
VIVESH <35293085+574n13y@users.noreply.github.com>
2025-04-04 21:56:39 +0000

    Copy file

---
+++ Tasks/Basic-Level/15th.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "To copy file \n use: cp source_file_name destination_file_name \n (seprated by spaces)"
+cp server.log server.log.backup
+echo -e 
+echo -e "To veiw files \n use ls -lrth \n"
+ls -lrth
