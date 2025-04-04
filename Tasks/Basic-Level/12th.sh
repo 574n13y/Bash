8faf604dd336f020fcf93ec84b28bd4f87644a31
VIVESH <35293085+574n13y@users.noreply.github.com>
2025-04-04 21:21:36 +0000

    create multipe directories

--- Tasks/Basic-Level/12th.sh
+++ Tasks/Basic-Level/12th.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo -e "Enter the names of the directories to create (separated by spaces): "
-read -r directory_names
+echo -e "Create multipe directories at once \n use - mkdir (seprated by spaces \n ):"
+mkdir infra monitoring deployment
+echo -e ""
+echo -e "To view dreated directories \n use- ls -lrth \n"
+ls -lrth
 
-echo -e "Creating directories: $directory_names\n"
-mkdir $directory_names
+######### ----------------------------------------------------------------- #####
+# Using user input
+#echo -e "Enter the names of the directories to create (separated by spaces): "
+#mkdir 
+#read -r directory_names # this is a advance way to ask use to name the directory and it will be created.
+#echo -e "Creating directories: $directory_names\n"
+#mkdir $directory_names
