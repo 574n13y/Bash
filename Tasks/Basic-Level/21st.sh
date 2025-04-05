ffca737997d49e9e51b959359029ce708cfce1b5
574n13y <vivesht@gmail.com>
2025-04-05 10:53:22 +0000

    Display content

---
+++ Tasks/Basic-Level/21st.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+
+echo -e "Display the content of file \n Use: cat command \n "
+file_name="app.config"
+echo -e "Content of $file_name : \n "
+
+cat $file_name
+
+########################################################################################################
+#### user input ####
+########################################################################################################
+
+#echo -e "Display the content of file \n Use: cat command \n "
+#read file_name
+
+#echo -e "Content of $file_name : \n "
+
+#if [ -f "$file_name" ]; then
+#  cat "$file_name"
+#else
+#  echo "Error: File '$file_name' does not exist."
+#fi
+
+########################################################################################################
+
