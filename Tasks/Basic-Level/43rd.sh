43ffdd71050dc6fe2e6332056d1238db7ba6f1b0
574n13y <vivesht@gmail.com>
2025-04-06 15:46:22 +0000

    for loop

---
+++ Tasks/Basic-Level/43rd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# For loop to print numbers from 1 to 5
+
+
+echo -e "\n--- For Loop: Numbers 1 to 5 ---\n"
+for i in 1 2 3 4 5; do
+  echo "Number: $i"
+done
+
+
+echo -e "\n--- Script Finished ---\n"
