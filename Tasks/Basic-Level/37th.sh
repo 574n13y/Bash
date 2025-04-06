10b6693a89fa223b948b7c037870ffcf6918e270
574n13y <vivesht@gmail.com>
2025-04-06 15:26:24 +0000

    check if file is executable

---
+++ Tasks/Basic-Level/37th.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+###################################################################################################
+#     	                         Manual / Hardcoded way                                           #
+###################################################################################################
+
+#file_to_check="your_file_name_here" # Replace with the actual file name
+
+#echo -e "Checking if '$file_to_check' is executable - Manual Way\n"
+
+#if [ -x "$file_to_check" ]; then
+#  echo "The file '$file_to_check' is executable."
+#else
+#  echo "The file '$file_to_check' is not executable (or does not exist)."
+#fi
+
+###################################################################################################
+#                                              User Input Way                                     #
+###################################################################################################
+
+echo -e "\n#####################################################################\n"
+echo -e "Checking if a user-specified file is executable - User Input Way\n"
+
+while true; do
+  read -p "Enter the name of the file to check for executablility (or 'q' to quit): " file_to_check_user
+  if [[ "$file_to_check_user" == "q" ]]; then
+    echo "Exiting."
+    exit 0
+  fi
+
+  if [ -f "$file_to_check_user" ]; then
+    if [ -x "$file_to_check_user" ]; then
+      echo "The file '$file_to_check_user' is executable."
+    else
+      echo "The file '$file_to_check_user' exists but is NOT executable."
+    fi
+    break # Exit after successful check (you can remove this if you want to check multiple files)
+  else
+    echo "Error: File '$file_to_check_user' not found. Please try again."
+  fi
+done
+
+echo -e "\nScript finished."
+
