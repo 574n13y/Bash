256a8b490c5fbeb99357f65561788cac69dcf45d
574n13y <vivesht@gmail.com>
2025-04-06 14:45:29 +0000

    use case of user input

---
+++ Tasks/Basic-Level/31st.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "Enter Server Name and store it in a Variables \n Use User input \n"
+read server_name
+echo -e "\n You have enterted $server_name as server name \n"
+
+
+#############################################################
+# You can now use the '$server_name' variable in your script
+# For example:
+# echo "Connecting to server: $server_name"
+# ssh user@"$server_name"
