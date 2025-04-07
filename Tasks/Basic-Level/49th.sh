5f0e05531a5261259addb1fd2f89ecd1b1be5a03
574n13y <vivesht@gmail.com>
2025-04-07 10:31:50 +0000

    greet functions

---
+++ Tasks/Basic-Level/49th.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the greet function
+greet() {
+  echo "Hello!"
+}
+
+# Main part of the script
+
+while true; do
+  echo -e "\nChoose an option:\n"
+  echo "1. Call the greet function"
+  echo "q. Quit"
+  read -p "Enter your choice: " choice
+
+  case "$choice" in
+    1)
+      greet   # Call the greet function
+      ;;
+    q)
+      echo "Exiting."
+      exit 0
+      ;;
+    *)
+      echo "Invalid choice. Please try again."
+      ;;
+  esac
+done
+
+echo -e "\nScript finished.\n"
